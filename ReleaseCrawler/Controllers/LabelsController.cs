5221a009535193d20a7bce103e3564a723613e91
Egor Shoba <egorshoba@outook.com>
2017-11-08 20:49:03 +0500

    Api project has been cleared of unnecessary files

--- ReleaseCrawler/Controllers/LabelsController.cs
+++ ReleaseCrawler/Controllers/LabelsController.cs
@@ -1,13 +1,9 @@
 ﻿using ReleaseCrowler.Models;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Web;
 using System.Web.Http;
-using System.Web.Mvc;
 
 namespace ReleaseCrowler.Controllers
 {
