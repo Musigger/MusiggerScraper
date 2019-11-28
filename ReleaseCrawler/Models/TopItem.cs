93f6491e33f4021a681fca8b65c5ef2130b05838
EgorShoba <egorshoba@outlook.com>
2019-11-28 18:09:30 +0500

    TopRelease model has been changed

--- ReleaseCrawler/Models/TopItem.cs
+++ ReleaseCrawler/Models/TopItem.cs
@@ -5,12 +5,12 @@ namespace ReleaseCrowler.Models
 {
     public class TopItem
     {
-        public ReleaseItem Release { get; set; }
+        public TopReleaseItem Release { get; set; }
         public decimal Goodness { get; set; }
 
         public TopItem(Release release)
         {
-            Release = new ReleaseItem(release);
+            Release = new TopReleaseItem(release);
             Goodness = Ranger.GetReleaseGoodness(release);
         }
     }
---
+++ ReleaseCrawler/Models/TopReleaseItem.cs
@@ -0,0 +1,24 @@
+﻿using ReleaseCrawler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReleaseCrowler.Models
+{
+    public class TopReleaseItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Label { get; set; }
+        public string Cover { get; set; }
+
+        public TopReleaseItem(Release release)
+        {
+            Id = release.Id;
+            Name = release.Name;
+            Label = release.Label;
+            Cover = release.Cover;
+        }
+    }
+}
